b6e3f2dd2fa8321a9952f6656021f26ff29dada9
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2023-01-23 22:28:41 +0530

    initial commits

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './intro.css';
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <div className="i">
+      <div className='i-left'>
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My Name is</h2>
+          <h1 className="i-name">Bajrang</h1>
+        </div>
+      </div>
+      <div className='i-right'>right</div>
+     
+    </div>
   )
 }
 
