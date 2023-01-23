1711064ebb94df03248f1287d456b434affbd477
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2023-01-23 14:34:54 +0530

    added basic rafce into the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import Toggle from "./components/toggle/Toggle";
+import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
+import ProductList from "./components/productList/ProductList";
+import Contact from "./components/contact/Contact";
+
+
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Toggle />
+      <Intro />
+      <About />
+      <ProductList />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './productList.css';
+
+const ProductList = () => {
+  return (
+    <div>ProductList</div>
+  )
+}
+
+export default ProductList
